a9d703d840f9c225ee25d046b1b69e0d54196cf3
nannom <88080149+nannom@users.noreply.github.com>
2024-11-28 11:14:31 +0900

    Update os.cpp

--- os.cpp
+++ os.cpp
@@ -1,17 +1,20 @@
 void main();
-extern "C" void _start() {
+extern "C" void _start() { //main을 호출
     main();
     return;
 }
 #define WIDTH 320
 #define HEIGHT 200
 
-//__asm__ __volatile__("mov %0, al;" :"=r"(keybuf) );
 void Write_port_byte(unsigned short port, unsigned char data) {
     __asm__ __volatile__ (
+        "cli;"
+        "pusha;"
         "mov dx, %0;"
         "mov al, %1;"
         "out dx, al;"
+        "popa;"
+        "sti;"
         :
         : "r" (port), "r" (data)
         : "dx", "al"
@@ -40,7 +43,7 @@ typedef unsigned int uint32_t;
 volatile char *video = (char*)(0xB8000);
 volatile unsigned short position = 0;
 volatile uint32_t _timer = 0;
-int printf(const char* _text,...) {
+int printf(const char* _text,...) { //단순 출력&커서 자동 옮김
     char* arg_ptr = (char*)(&_text) + sizeof(_text);
     for(int i = 0;_text[i] != '\0';i++) {
         if(_text[i] == '%') {
@@ -131,7 +134,7 @@ int printf(const char* _text,...) {
     Write_port_byte(0x3D5,(unsigned char)((position >> 8) & 0xFF));
     return 0;
 }
-char issame(const char* _text1,const char* _text2) {
+char issame(const char* _text1,const char* _text2) { //콘솔에서 명령어 감지할 때 사용
     while(*_text1 != 0 && *_text2 != 0) {
         if(*_text1 != *_text2) {
             return 0;
@@ -145,114 +148,7 @@ char issame(const char* _text1,const char* _text2) {
 volatile int kbd_state[256] = {
     0,
 };
-unsigned char transScan(unsigned char target)
-{
-	unsigned char result;
 
-	switch (target) 
-	{
-    case 0x01: result = '\0'; break; //ESC
-    case 0x02: result = '1'; break;
-    case 0x03: result = '2'; break;
-    case 0x04: result = '3'; break;
-    case 0x05: result = '4'; break;
-    case 0x06: result = '5'; break;
-    case 0x07: result = '6'; break;
-    case 0x08: result = '7'; break;
-    case 0x09: result = '8'; break;
-    case 0x0A: result = '9'; break;
-    case 0x0B: result = '0'; break;
-    case 0x0C: result = '-'; break;
-    case 0x0D: result = '='; break;
-	case 0x1E: result = 'a'; break;
-	case 0x30: result = 'b'; break;
-	case 0x2E: result = 'c'; break;
-	case 0x20: result = 'd'; break;
-	case 0x12: result = 'e'; break;
-	case 0x21: result = 'f'; break;
-	case 0x22: result = 'g'; break;
-	case 0x23: result = 'h'; break;
-	case 0x17: result = 'i'; break;
-	case 0x24: result = 'j'; break;
-	case 0x25: result = 'k'; break;
-	case 0x26: result = 'l'; break;
-	case 0x32: result = 'm'; break;
-	case 0x31: result = 'n'; break;
-	case 0x18: result = 'o'; break;
-	case 0x19: result = 'p'; break;
-	case 0x10: result = 'q'; break;
-	case 0x13: result = 'r'; break;
-	case 0x1F: result = 's'; break;
-	case 0x14: result = 't'; break;
-	case 0x16: result = 'u'; break;
-	case 0x2F: result = 'v'; break;
-	case 0x11: result = 'w'; break;
-	case 0x2D: result = 'x'; break;
-	case 0x15: result = 'y'; break;
-	case 0x2C: result = 'z'; break;
-	case 0x39: result = ' '; break; // 스페이스
-    case 0x0E: result = 0x08; break; // 백스페이스 아스키코드 = 8
-    case 0x29: result = '`'; break;
-    case 0x1C: result = '\n'; break;
-	default: result = 0xFF; break; 
-		// 구현안된 것은 무시한다. 구분자는 0xFF
-	}
-	return result;
-}
-unsigned char TransScan(unsigned char target)
-{
-	unsigned char result;
-
-	switch (target) 
-	{
-    case 0x01: result = '\0'; break; //ESC
-    case 0x02: result = '!'; break;
-    case 0x03: result = '@'; break;
-    case 0x04: result = '#'; break;
-    case 0x05: result = '$'; break;
-    case 0x06: result = '%'; break;
-    case 0x07: result = '^'; break;
-    case 0x08: result = '&'; break;
-    case 0x09: result = '*'; break;
-    case 0x0A: result = '('; break;
-    case 0x0B: result = ')'; break;
-    case 0x0C: result = '_'; break;
-    case 0x0D: result = '+'; break;
-	case 0x1E: result = 'A'; break;
-	case 0x30: result = 'B'; break;
-	case 0x2E: result = 'C'; break;
-	case 0x20: result = 'D'; break;
-	case 0x12: result = 'E'; break;
-	case 0x21: result = 'F'; break;
-	case 0x22: result = 'G'; break;
-	case 0x23: result = 'H'; break;
-	case 0x17: result = 'I'; break;
-	case 0x24: result = 'J'; break;
-	case 0x25: result = 'K'; break;
-	case 0x26: result = 'L'; break;
-	case 0x32: result = 'M'; break;
-	case 0x31: result = 'N'; break;
-	case 0x18: result = 'O'; break;
-	case 0x19: result = 'P'; break;
-	case 0x10: result = 'Q'; break;
-	case 0x13: result = 'R'; break;
-	case 0x1F: result = 'S'; break;
-	case 0x14: result = 'T'; break;
-	case 0x16: result = 'U'; break;
-	case 0x2F: result = 'V'; break;
-	case 0x11: result = 'W'; break;
-	case 0x2D: result = 'X'; break;
-	case 0x15: result = 'Y'; break;
-	case 0x2C: result = 'Z'; break;
-	case 0x39: result = ' '; break; // 스페이스
-    case 0x0E: result = 0x08; break; // 백스페이스 아스키코드 = 8
-    case 0x29: result = '~'; break;
-    case 0x1C: result = '\n'; break;
-	default: result = 0xFF; break; 
-		// 구현안된 것은 무시한다. 구분자는 0xFF
-	}
-	return result;
-}
 struct IDT {
     uint16_t offset_low;
     uint16_t selector;
@@ -277,6 +173,15 @@ char scan_code_table[] = {
     '*', 0, ' '};
 void idt_ignore() {
     __asm__ __volatile__(
+        "cli;"
+        "pusha;"
+        "mov dx, 0x20;"
+        "mov al, 0x20;"
+        "out dx, al;"
+        "mov dx, 0xA0;"
+        "out dx, al;"
+        "popa;"
+        "sti;"
         "iretd;"
     );
 }
@@ -285,24 +190,12 @@ void idt_timer() {
     __asm__ __volatile__(
         "cli;"
         "pusha;"
-        "push ds;"
-        "push es;"
-        "push fs;"
-        "push gs;"
-        "mov ax, 0x08;" 
-        "mov ds, ax;"
-        "mov es, ax;"
-        "mov fs, ax;"
-        "mov gs, ax;"
     );
     _timer++;
-    __asm__ __volatile__(
+    __asm__ __volatile__ (
+        "mov dx, 0x20;"
         "mov al, 0x20;"
-        "out 0x20, al;"
-        "pop gs;"
-        "pop fs;"
-        "pop es;"
-        "pop ds;"
+        "out dx, al;"
         "popa;"
         "sti;"
         "iretd;"
@@ -313,22 +206,15 @@ void idt_keyboard() {
     __asm__ __volatile__(
         "cli;"                // 인터럽트 비활성화
         "pusha;"              // 모든 레지스터 저장
-        "mov ax, 0x08;"       // 코드 세그먼트로 변경 (플랫폼에 따라 다를 수 있음)
-        "mov ds, ax;"         // DS 변경
-        "mov es, ax;"         // ES 변경
-        "mov fs, ax;"         // FS 변경
-        "mov gs, ax;"         // GS 변경
-        "in al, 0x60;"        // 키보드 데이터 포트에서 데이터 읽기
     );
     printf("test");
 
-    unsigned char keybuf;
-    __asm__ __volatile__("mov %0, al;" : "=r"(keybuf));  // AL 레지스터에서 키 코드 저장
+    unsigned char keybuf = Read_port_byte(0x60);
     printf("%c", scan_code_table[keybuf]);
-    Write_port_byte(0x20, 0x20);
-    __asm__ __volatile__(
-        "mov al, 0x20;"           // PIC에 EOI 신호
-        "out 0x20, al;"           // EOI 신호 전송
+    __asm__ __volatile__ (
+        "mov dx, 0x20;"
+        "mov al, 0x20;"
+        "out dx, al;"
         "popa;"                   // 모든 레지스터 복원
         "sti;"                    // 인터럽트 활성화
         "iretd;"                  // 인터럽트 처리 종료, 이전 상태로 복귀
@@ -376,7 +262,8 @@ void init_idt() {
 
     uint32_t ptr = (uint32_t)idt_ignore;
     for(int i = 0;i<256;i++) {
-        if(i != 0x21) {
+        if(i != 0x21 && i != 0x20) 
+        {
             idt[i].offset_low = ptr & 0xFFFF;
             idt[i].selector = 0x08;
             idt[i].zero = 0;
@@ -384,20 +271,13 @@ void init_idt() {
             idt[i].offset_high = (ptr >> 16) & 0xFFFF;
         }
     }
-    // IDT 엔트리 설정
-    //ptr = (uint32_t)idt_ignore;
-    //idt[0].offset_low = ptr & 0xFFFF;
-    //idt[0].selector = 0x08;
-    //idt[0].zero = 0;
-    //idt[0].type_attr = 0x8E;
-    //idt[0].offset_high = (ptr >> 16) & 0xFFFF;
 
-    //ptr = (uint32_t)idt_timer;
-    //idt[0x20].offset_low = ptr & 0xFFFF;
-    //idt[0x20].selector = 0x08;
-    //idt[0x20].zero = 0;
-    //idt[0x20].type_attr = 0x8E;
-    //idt[0x20].offset_high = (ptr >> 16) & 0xFFFF;
+    ptr = (uint32_t)idt_timer;
+    idt[0x20].offset_low = ptr & 0xFFFF;
+    idt[0x20].selector = 0x08;
+    idt[0x20].zero = 0;
+    idt[0x20].type_attr = 0x8E;
+    idt[0x20].offset_high = (ptr >> 16) & 0xFFFF;
 
     ptr = (uint32_t)idt_keyboard;
     idt[0x21].offset_low = ptr & 0xFFFF;
@@ -423,95 +303,7 @@ int help() {
     printf("\n<help> get commands\n<cls> clear screen\n<WaSans> WaSans!!!\n");
     return 0;
 }
-int tetris() {
-    cls();
-    char blocks[20][10] = {
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0}
-    };
-    int x = 0,y = 0;
-    int p_x = 3,p_y = 0;
-    int timer = *((int*)0x30000) + 1000;
-    while(1) {
-        for(y = 0;y<20;y++) {
-            *((char*)(0xB8018) + ((y + 4) * 160)) = ' ';
-            *((char*)(0xB8018) + ((y + 4) * 160) + 1) = 0x70;
-            *((char*)(0xB8018) + ((y + 4) * 160) + 2) = ' ';
-            *((char*)(0xB8018) + ((y + 4) * 160) + 3) = 0x70;
-            *((char*)(0xB8018) + ((y + 4) * 160) + 44) = ' ';
-            *((char*)(0xB8018) + ((y + 4) * 160) + 45) = 0x70;
-            *((char*)(0xB8018) + ((y + 4) * 160) + 46) = ' ';
-            *((char*)(0xB8018) + ((y + 4) * 160) + 47) = 0x70;
-        }
-        for(x = 0;x<12;x++) {
-            *((char*)(0xB8018) + (24 * 160) + (x * 4)) = ' ';
-            *((char*)(0xB8018) + (24 * 160) + (x * 4) + 1) = 0x70;
-            *((char*)(0xB8018) + (24 * 160) + (x * 4) + 2) = ' ';
-            *((char*)(0xB8018) + (24 * 160) + (x * 4) + 3) = 0x70;
-        }
-        for(y = 0;y<20;y++) {
-            for(x = 0;x<10;x++) {
-                *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4)) = ' ';
-                *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 1) = blocks[y][x] * 0x10;
-                *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 2) = ' ';
-                *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 3) = blocks[y][x] * 0x10;
-                if(y == p_y && x == p_x) {
-                    *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4)) = ' ';
-                    *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 1) = 1 * 0x10;
-                    *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 2) = ' ';
-                    *((char*)(0xB801C) + ((y + 4) * 160) + (x * 4) + 3) = 1 * 0x10;
-                }
-            }
-        }
-            while((*((unsigned int*)0x30000)) < timer) {
-            }
-            timer += 1000;
-            p_y += 1;
-    }
-    cls();
-    return 0;
-}
-
-/*
-0x00 quit
-0x01 print
-0x02 scan
-0x03 if
-0x04 iszero
-0x05 islow
-0x06 ishigh
-0x07 plus
-0x08 set
-0x09 jmp
-0xFF none
-*/
 void main() {
-    //initPIC();
-        const char hx[] = {
-        '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
-    };
-    //unsigned char testcode[] = {0x02,0x1E,0x00,0x00,0x00,0x48,0x03,0x00,0x00,0x00,0x48,0x00,0x00,0x00,0x49,0x04,0x00,0x00,0x00,0x27,0x03,0x00,0x00,0x00,0x4A,0x00,0x00,0x00,0x49,0x04,0x00,0x00,0x00,0x35,0x09,0x00,0x00,0x00,0x00,0x08,0x00,0x00,0x00,0x4A,0x00,0x00,0x00,0x49,0x09,0x00,0x00,0x00,0x00,0x08,0x00,0x00,0x00,0x4A,0x00,0x00,0x00,0x4B,0x01,0x00,0x00,0x00,0x4C,0x09,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,'A'};
-    //                          0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F    10   11   12   13   14   15   16   17   18   19   1A   1B   1C   1D   1E   1F   20   21   22   23   24   25   26   27   28   29   2A   2B   2C   2D   2E   2F   30   31   32   33   34   35   36   37   38   39   3A   3B   3C   3D   3E   3F   40   41   42   43   44   45   46   47   48   49   4A   4B   4C
-    //                          0    1    2    3    4    5    6    7    8    9    10   11   12   13   14   15   16   17   18   19   20   21   22   23   24   25   26   27   28   29   30   31   32   33   34   35   36   37   38   39   40   41   42   43   44   45   46   47   48   49   50   51   52   53   54   55   56   57   58   59   60   61   62   63   64   65   66   67   68   69   70   71   72   73   74   75   76
-    
     int i = 0;
     for(i = 0;i<256;i++) {
         kbd_state[i] = 0;
@@ -523,23 +315,24 @@ void main() {
         *v = 0;
     }
     printf("<help> to get help!\n>");
-    init_pic();  // PIC 초기화
     //init_pit();  // PIT 초기화
-    //init_idt();  // IDT 초기화
-    *((char*)(0x20000)) = (char)0;
-    //*((char*)(0x20001)) = (char)0;
+    init_pic();  // PIC 초기화
+    init_idt();  // IDT 초기화
+    //__asm__ __volatile__("int 0x21"); //인터럽트 테스트를 위해 실행
     int time = 10000;
     while(1) {
-        if(Read_port_byte(0x64) & 1) {
+        /*
+        if(Read_port_byte(0x64) & 1) { //키보드 입력 받는 부분
             //__asm__ __volatile__("int 0x21");
             char data = Read_port_byte(0x60);
             if(!(data & 0x80)) {
                 printf("%c", scan_code_table[data]);
             }
         }
-        for(i = 0;i<128 && 0;i++) {
+        */
+        for(i = 0;i<128 && 0;i++) { //콘솔 관련(미사용중)
             if(kbd_state[i] == 1) {
-                if(transScan(i) == '\n') {
+                if(scan_code_table[i] == '\n') {
                     int d = (int)video - 0xB8000;
                     d -= d % 160;
                     d += 0xB8000;
@@ -549,18 +342,12 @@ void main() {
                     else if(issame(((char*)d) + 2, "cls") == 1) {
                         cls();
                     }
-                    else if(issame(((char*)d) + 2, "test") == 1) {
-                        //int code_ = process.point;
-                    }
-                    else if(issame(((char*)d + 2), "tetris") == 1) {
-                        tetris();
-                    }
                     else {
                         printf("\nunknowing command");
                     }
                     printf("\n>");
                 }
-                else if(transScan(i) == '\b') {
+                else if(scan_code_table[i] == '\b') {
                     if(video > (char*)0xB8000) {
                         video -= 2;
                         position--;
@@ -584,16 +371,16 @@ void main() {
                 else if(i == 42 || i == 54) {
                 }
                 else {
-                    if(transScan(i) == 0xFF) {
+                    if(scan_code_table[i] == 0xFF) {
                         //printf("%c", hx[i >> 4]);
                         //printf("%c", hx[i & 0xF]);
                     }
                     else {
                         if(kbd_state[42] == 0 && kbd_state[54] == 0) {
-                            printf("%c", transScan(i));
+                            printf("%c", scan_code_table[i]);
                         }
                         else {
-                            printf("%c", TransScan(i));
+                            printf("%c", scan_code_table[i]);
                         }
                     }
                 }
